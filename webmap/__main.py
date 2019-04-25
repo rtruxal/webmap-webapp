156c633370b21c17160345ad2f1ba96f3d911b21
Rob <rtruxal@gmail.com>
2019-04-25 13:39:49 -0700

    license

--- webmap/__main.py
+++ webmap/__main.py
@@ -9,7 +9,9 @@ bp = Blueprint('/webmap', __name__, 'webmap')
 @bp.route('/', methods=('GET', 'POST'))
 def webmap():
     if request.method == 'POST':
-        #TODO:
+        #TODO: Create either a click-interface, or a form-interface to allow users to interact with the graph visualization. 
+        #TODO: By default, use my Free-tier Bing API key & limit queries based on user-session & absolute frequency.
+        #TODO: Provide a form through which a user can specify their own API key & remove afformentioned query limits.
         pass
     return render_template('viz.html')
 
